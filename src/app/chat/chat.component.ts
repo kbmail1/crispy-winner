dc0b364a0965d533d0a6c064e711ae4e05ae0b0d
kundan Bapat <kundan.bapat@gmail.com>
2022-01-30 18:56:10 -0500

    chat app init 3

--- src/app/chat/chat.component.ts
+++ src/app/chat/chat.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterContentInit, Component, ElementRef, OnInit, AfterContentChecked, ViewChild } from '@angular/core';
 import AuthService from '../services/auth.service'
 import axios from 'axios'
 
@@ -7,7 +7,9 @@ import axios from 'axios'
   templateUrl: './chat.component.html',
   styleUrls: ['./chat.component.scss']
 })
-export class ChatComponent implements OnInit {
+export class ChatComponent implements OnInit, AfterContentInit {
+
+  @ViewChild('signupCode') signupCode!: ElementRef<HTMLInputElement>;
 
   // put this in a state object so false/true settings are correct */
   showSignupForm: boolean = false
@@ -15,9 +17,12 @@ export class ChatComponent implements OnInit {
   showEnterCodeForm: boolean = false
   accountCreated: boolean = false
 
+  static MaxSignupAttempts = 3
+  errorMsg = ''
+  signupAttempts = 0
   loggedIn = false
-  email: string = ''
   password: string = ''
+  email: string = ''
   code: string = ''
 
   constructor(
@@ -28,9 +33,14 @@ export class ChatComponent implements OnInit {
     this.showEnterCodeForm = false
     this.accountCreated = false
     this.loggedIn = false
+
   }
 
   ngOnInit(): void {
+  }
+
+  ngAfterContentInit() {
+    this.errorMsg = ''
   }
 
   doShowNoForm(): boolean {
@@ -38,7 +48,6 @@ export class ChatComponent implements OnInit {
       !this.showEnterCodeForm &&
       !this.showLoginForm &&
       !this.showSignupForm &&
-      !this.accountCreated &&
       !this.loggedIn
     )
   }
@@ -65,17 +74,40 @@ export class ChatComponent implements OnInit {
     this.loggedIn = false
   }
 
-
-
   handleValidateSignupCode = () => {
-    // yay
-    console.log('in validateSignupCode() handler')
-    this.accountCreated = true
+    console.log('in validateSignupCode() handler - does email exist in local storage?')
+    this.accountCreated = false
 
     this.showSignupForm = false
     this.showLoginForm = false
     this.showEnterCodeForm = false
     this.loggedIn = false
+
+    const userEnteredCode = this.signupCode.nativeElement.value
+
+    let sessionCode = sessionStorage.getItem(this.email)
+    if (sessionCode == null) {
+      console.log(`invalid-email ${this.email} or code expired`)
+    }
+
+    if (userEnteredCode === sessionCode) {
+      console.log('signup successful')
+      sessionStorage.removeItem(this.email)
+      this.accountCreated = true
+    } else {
+      // setTimeout(() => sessionStorage.removeItem(this.email), 5 * 60 * 1000)
+      this.errorMsg = 'Code you entered is either invalid or expired.  Try again'
+      this.showSignupForm = true // again (timeout and go back to
+      if (ChatComponent.MaxSignupAttempts === this.signupAttempts) {
+        this.errorMsg = 'Exceeded maximum signup attempts. Try again later'
+        sessionStorage.removeItem(this.email)
+        this.signupAttempts = 0
+        this.showSignupForm = false
+      }
+      this.signupAttempts += 1
+      // home page after few attempts
+      // show failed account creation msg; retry - send email again
+    }
   }
 
   handleSignUp = () => {
@@ -89,13 +121,12 @@ export class ChatComponent implements OnInit {
     console.log('signup')
 
     // receive email in form as argument.
-    let email = 'kundan.bapat@gmail.com'
     let code = '' + Math.floor(Math.random() * 10000)
-    localStorage.setItem(email, code)
-    setTimeout(() => { localStorage.removeItem(email) }, 5 * 60 * 1000)
+    // local storage persistent. only want 'this browser tab' persistence.
+    sessionStorage.setItem(this.email, code)
 
     axios.post('https://localhost:8888/signup', {
-      email,
+      email: this.email,
       code,
     })
       .then(() => {
@@ -115,4 +146,16 @@ export class ChatComponent implements OnInit {
 
     // TODO: Decide what to show If Login success or failure.
   }
+
+  handleLogout = () => {
+    console.log('in logout() handler')
+    this.loggedIn = false
+
+    this.showLoginForm = false
+
+    this.showEnterCodeForm = false
+    this.showSignupForm = false
+    this.accountCreated = false
+  }
+
 }
